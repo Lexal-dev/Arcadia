d0dee4f4f5a69e8f18c92efd57cfe91de65d338c
Lexal-dev <landoltalexis67@gmail.com>
2024-05-09 12:15:56 +0200

    Add basic squeletton for presentation homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,71 @@
 
 export default function Home() {
   return (
-    <main className="h-full w-full text-center">
-      
+    <main className="h-full w-full text-right flex flex-col items-center">
+
+      <h2 className="font-mono text-4xl my-5 font-bold underline">Le ZOO : Arcadia</h2>
+      <p className="p-5 text-start mb-3 w-2/3 font-mono text-xl">
+        Découvrez le zoo Arcadia,
+        un joyau niché près de la légendaire forêt de Brocéliande en Bretagne depuis 1960.
+        Immergez-vous dans un monde où la nature s'épanouit et où les animaux prospèrent dans des habitats soigneusement aménagés,
+        tels que la savane, la jungle et les marais.
+      </p>
+
+      <p className="p-5 text-start mb-3 w-2/3 font-mono text-xl">
+        Au cœur de cet écosystème, chaque jour, une équipe dévouée de vétérinaires veille scrupuleusement à la santé et au bien-être de chaque pensionnaire.
+        Des contrôles minutieux sont effectués avant l'ouverture du zoo, garantissant ainsi des conditions optimales pour nos amis à fourrure et à plumes.
+        De plus, la nutrition est méticuleusement calculée, chaque gramme étant soigneusement pesé selon les recommandations des experts.
+      </p>
+
+      <p className="p-5 text-start mb-3 w-2/3 font-mono text-xl">
+        Porté par la vision ambitieuse de son directeur, José, le zoo Arcadia se distingue également par sa santé financière florissante.
+        Mais au-delà des chiffres, c'est le bonheur des animaux qui fait la fierté de toute l'équipe.  
+      </p>
+
+      <p className="p-5 text-start mb-3 w-2/3 font-mono text-xl">
+        Aujourd'hui, Arcadia embrasse une nouvelle ère avec l'aspiration de José à offrir une expérience encore plus immersive à ses visiteurs.
+        C'est ainsi que votre expertise en tant que nouveau membre de l'équipe DevSoft,
+        sélectionnée pour réaliser une application web innovante, entre en scène.
+        Avec cette application, les visiteurs pourront explorer les merveilles du zoo,
+        découvrir l'état de santé de leurs animaux préférés et accéder facilement aux services et aux horaires du parc.
+      </p>
+
+      <p className="p-5 text-start mb-3 w-2/3 font-mono text-xl">
+        Bienvenue dans l'aventure Arcadia, où la technologie se met au service de la nature pour une expérience inoubliable.
+      </p>
+
+      <div className="font-caption text-4xl my-8">//BANNIERE POUR FAIRE UNE TRANSITION//</div>
+
+      <h3>Animaux et Habitats</h3>
+      <p className="font-mono text-start p-5 mb-2 w-2/3 text-xl">
+        Les habitats naturels offrent un tableau éclectique de la biodiversité,
+        où chaque écosystème abrite une multitude d'espèces fascinantes.
+        Explorez avec nous trois habitats distincts et rencontrez deux des incroyables créatures qui les appellent chez eux.
+      </p>
+
+      <div className="font-caption text-2xl text-bold mb-3">La Savane</div>
+      <p className="font-mono text-start p-5 mb-2 w-2/3 text-xl">
+        La vaste étendue de la savane évoque des images de vastes plaines ondulantes et de majestueux acacias.
+        C'est un monde où le lion règne en souverain, avec sa crinière dorée et sa puissance tranquille.
+        Dans ce royaume, les troupeaux d'éléphants se déplacent gracieusement, tandis que les girafes se penchent pour se nourrir des feuilles les plus hautes des arbres.
+      </p>
+
+      <div className="font-caption text-2xl text-bold mb-3">La Jungle</div>
+      <p className="font-mono text-start p-5 mb-2 w-2/3 text-xl">
+        La jungle est un monde de vert dense et de vie exubérante, où chaque recoin est une explosion de vie. Ici, le jaguar, un prédateur agile et furtif, se fond dans le feuillage dense, prêt à saisir sa proie avec une précision redoutable.
+        Parmi les lianes suspendues et les feuilles épaisses, le toucan aux couleurs éclatantes éclaire le ciel de son bec emblématique, apportant une touche de splendeur tropicale.
+      </p>
+
+      <div className="font-caption text-2xl text-bold mb-3">Les Marais</div>
+      <p className="font-mono text-start p-5 mb-2 w-2/3 text-xl">
+        Les marais sont des terres mystérieuses où l'eau et la terre se mêlent dans une danse constante. C'est l'habitat idéal pour l'alligator, un prédateur redoutable qui se glisse silencieusement dans les eaux sombres, attendant patiemment sa proie.
+        Parmi les roseaux ondulants et les nénuphars flottants, l'héron cendré se tient immobile, son regard perçant guettant le moindre mouvement dans l'eau.
+      </p>
+
+      <div className="font-caption text-4xl my-8">//BANNIERE POUR FAIRE UNE TRANSITION//</div>
+
+      <div className="font-caption text-2xl text-bold mb-3">Espace Commentaires</div>
+      <div>futur block de commentaires</div>
     </main>
   );
 }
